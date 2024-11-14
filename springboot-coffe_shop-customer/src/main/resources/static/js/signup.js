8c50f83232b33654a66445ed6dcb9a6a1ac33fa6
thanhnhi1208 <nguyenthanhnhi310718@gmail.com>
2024-11-14 16:12:54 +0700

    init project

--- springboot-coffe_shop-customer/src/main/resources/static/js/signup.js
+++ springboot-coffe_shop-customer/src/main/resources/static/js/signup.js
@@ -1,8 +1,8 @@
-document.addEventListener('DOMContentLoaded', function(){
-	document.getElementById('a-send-otp').addEventListener('click', function(){
+document.addEventListener('DOMContentLoaded', function() {
+	document.getElementById('a-send-otp').addEventListener('click', function() {
 		let email = document.getElementById('email').value;
-		if(email != '' && email.includes('@gmail.com')){
-			let url = '/customer/signup/sendOtpToEmail?email='+email;
+		if (email != '' && email.includes('@gmail.com')) {
+			let url = '/customer/signup/sendOtpToEmail?email=' + email;
 			console.log(email)
 			fetch(url);
 		}
